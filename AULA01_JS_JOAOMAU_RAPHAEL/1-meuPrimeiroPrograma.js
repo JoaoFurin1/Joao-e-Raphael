cbc8f0a32b827bb595358a425ccbcb69299881d4
João Furini <joao78furini@gmail.com>
2024-09-06 08:58:29 -0300

    Add files via upload

---
+++ AULA01_JS_JOAOMAU_RAPHAEL/1-meuPrimeiroPrograma.js
@@ -0,0 +1,16 @@
+//primeira instrução em JS
+console.log("\nOlá mundo cruel da programação\n");
+
+//alunos 1 e 2 são variáveis
+const aluno1="João";
+const aluno2="Raphael"; 
+
+const idadeAluno1 = 15;
+const idadeAluno2 = 16;
+
+const somaIdades = idadeAluno1+idadeAluno2;
+
+const dupla = aluno1+" e "+aluno2;
+
+console.log("somos a dupla: "+dupla+"\n");
+console.log("a soma de nossas idades é: "+somaIdades+" anos");
