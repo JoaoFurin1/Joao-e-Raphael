dc20340f1dd987aa7faf4a46e45daeb1e47b36cd
João Furini <joao78furini@gmail.com>
2024-09-18 09:53:51 -0300

    Add files via upload

---
+++ AULA01_JS_JOAOMAU_RAPHAEL/script.js
@@ -0,0 +1,13 @@
+//alert("Testando o script!");
+
+var nome = prompt("Qual o seu nome? ");
+
+var sobrenome = prompt("Qual o seu sobrenome? ");
+
+console.log("Olá "+nome+" "+sobrenome);
+
+//saída no DOM (modelo de objeto do documento)
+
+document.write("<h1>Bem vindo "+nome+" "+sobrenome+"</h1>");
+
+document.write('<img src="https://sujeitoprogramador.com/steve.png" alt="foto do Steve"/>');
