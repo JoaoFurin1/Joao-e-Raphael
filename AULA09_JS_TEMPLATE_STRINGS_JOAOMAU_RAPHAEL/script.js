5686a8648e2bb799ce32e1ae2921615d1402413c
João Furini <joao78furini@gmail.com>
2024-10-28 08:22:28 -0300

    Add files via upload

---
+++ AULA09_JS_TEMPLATE_STRINGS_JOAOMAU_RAPHAEL/script.js
@@ -0,0 +1,9 @@
+let nome = 'João';
+let sobreNome = 'Furini';
+let idade = 15;
+
+let mensagem = 'Meu nome é ' +nome+ ' ' +sobreNome+ ' e minha idade é ' +idade+ ' anos.';
+console.log(mensagem);
+
+let mensagem2 = `Meu nome é ${nome} ${sobreNome} e minha idade é ${idade} anos.`
+console.log(mensagem2)
